195b8b3afa101164b9748844011fc0cd4af9ade7
Billy Novrando <novrandobillt@gmail.com>
2020-04-13 13:24:30 +0700

    Create World, Border, and testing object (rectangle)

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hi there!!!');
+const { World, Engine, Runner, Render, Bodies } = Matter;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+	element: document.body,
+	engine: engine,
+	options: {
+		width: 800,
+		height: 600
+	}
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+// Walls
+const walls = [
+	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+	Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+World.add(world, walls);
+World.add(world, Bodies.rectangle(400, 400, 60, 60));
